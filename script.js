b60c2b173b67244ab61f621fb5985b93955b83f7
Djamel IDIRI <djamel.idiri@hotmail.com>
2024-05-17 22:29:41 +0200

    effectuer une requête  avec fetch

--- script.js
+++ script.js
@@ -4,6 +4,44 @@ cart = document.querySelector('.cart-container');
 cartmenu = document.querySelector('#cart-menu');
 
 
+search.addEventListener('click', () => {
+    const departureInput = document.querySelector('#departure-input');
+    const arrivalInput = document.querySelector('#arrival-input');
+    const calendarInput = document.querySelector('#calendar-input');
+    
+
+    // Récupérer les valeurs des champs de saisie
+    const departure = departureInput.value;
+    const arrival = arrivalInput.value; 
+    const date = calendarInput.value;
+
+    // http://localhost:3000/trips?departure=Paris&arrival=Lyon&date=2024-05-15
+    // Effectuer une requête  avec fetch
+    fetch(`http://localhost:3000/trips?departure=${departure}&arrival=${arrival}&date=${date}`)
+    .then(response => response.json())
+    .then(data => {
+        // Traiter les données de réponse
+        if (data) {
+            console.log(data);
+            data.forEach(trip => {
+                // Créer des éléments HTML pour afficher chaque trajet
+                result.innerHTML += `
+                    <div id="result">
+                    <span id="trip">${departure} > ${arrival}</span>
+                    <span id="time">16:23</span>
+                    <span id="price">126€</span>
+                    <button id="book-button">Book</button>
+                    </div>`;
+            });
+        }
+    })
+        
+});
+
+
+
+
+
 
 // notrip found result
 
