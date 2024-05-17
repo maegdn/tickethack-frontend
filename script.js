33023c6877ad55032d01c043aca8f491cfe945da
Maelys Guardini <louguardini@gmail.com>
2024-05-17 21:08:39 +0200

    modification index, cart et bookings, test d'ajout DOM + css, suppression des fichiers js, retour à script.js

---
+++ script.js
@@ -0,0 +1,122 @@
+search = document.querySelector('#search-button');
+result = document.querySelector('.right-box');
+cart = document.querySelector('.cart-container');
+cartmenu = document.querySelector('#cart-menu');
+
+
+
+// notrip found result
+
+// search.addEventListener('click', () => {
+// result.innerHTML = `
+// <div>
+// <img id="notfound-image" src="images/notfound.png">
+// </div>
+// <hr id="green-line">
+// <p id="notrip-phrase">No trip found.</p>
+// `
+// })
+
+// trips found and list
+
+// search.addEventListener('click', () => {
+//     result.innerHTML = `
+//     <div id="result">
+//     <span id="trip">Paris > Lyon</span>
+//     <span id="time">16:23</span>
+//     <span id="price">126€</span>
+//     <button id="book-button">Book</button>
+//     </div>
+
+//     <div id="result">
+//     <span id="trip">Paris > Lyon</span>
+//     <span id="time">16:23</span>
+//     <span id="price">126€</span>
+//     <button id="book-button">Book</button>
+//     </div>
+
+//     <div id="result">
+//     <span id="trip">Paris > Lyon</span>
+//     <span id="time">16:23</span>
+//     <span id="price">126€</span>
+//     <button id="book-button">Book</button>
+//     </div>
+
+//     <div id="result">
+//     <span id="trip">Paris > Lyon</span>
+//     <span id="time">16:23</span>
+//     <span id="price">126€</span>
+//     <button id="book-button">Book</button>
+//     </div>
+
+//     <div id="result">
+//     <span id="trip">Paris > Lyon</span>
+//     <span id="time">16:23</span>
+//     <span id="price">126€</span>
+//     <button id="book-button">Book</button>
+//     </div>
+
+//     <div id="result">
+//     <span id="trip">Paris > Lyon</span>
+//     <span id="time">16:23</span>
+//     <span id="price">126€</span>
+//     <button id="book-button">Book</button>
+//     </div>
+
+//     <div id="result">
+//     <span id="trip">Paris > Lyon</span>
+//     <span id="time">16:23</span>
+//     <span id="price">126€</span>
+//     <button id="book-button">Book</button>
+//     </div>
+
+//     <div id="result">
+//     <span id="trip">Paris > Lyon</span>
+//     <span id="time">16:23</span>
+//     <span id="price">126€</span>
+//     <button id="book-button">Book</button>
+//     </div>
+
+//     <div id="result">
+//     <span id="trip">Paris > Lyon</span>
+//     <span id="time">16:23</span>
+//     <span id="price">126€</span>
+//     <button id="book-button">Book</button>
+//     </div>
+
+//     `
+//     })
+
+
+
+// CART
+
+// cart.addEventListener('click', () => {
+// cart.innerHTML = `
+//    <div id="cart-result">
+//     <span id="trip">Paris > Lyon</span>
+//     <span id="time">16:23</span>
+//     <span id="price">126€</span>
+//     <button id="cart-delete">X</button>
+//     </div>
+
+// // `
+// })
+
+// cart.addEventListener('click', () => {
+// cart.innerHTML = `
+// <p>My cart</p>
+// <div id="cart-result">
+// <span id="trip">Paris > Lyon</span>
+// <span id="time">16:23</span>
+// <span id="price">126€</span>
+// <button id="cart-delete">X</button>
+// </div>
+
+// <div id="total-container">
+// <div id="total-content">
+// <span>Total: 544€ </span>
+// <button id="purchase-button">Purchase</button>
+// </div>
+// </div>
+// `})
