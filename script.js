ea21cc9e814bac21f37a813b462091bf568e57ed
Djamel IDIRI <djamel.idiri@hotmail.com>
2024-05-18 10:05:52 +0200

    ajout du message No trip found dans le script en cas de trajet non trouvé

--- script.js
+++ script.js
@@ -13,37 +13,46 @@ search.addEventListener('click', () => {
 
     // Récupérer les valeurs des champs de saisie
     const departure = departureInput.value;
-    const arrival = arrivalInput.value; 
+    const arrival = arrivalInput.value;
     const date = calendarInput.value;
 
     // http://localhost:3000/trips?departure=Paris&arrival=Lyon&date=2024-05-15
     // Effectuer une requête  avec fetch
 
     fetch(`http://localhost:3000/trips?departure=${departure}&arrival=${arrival}&date=${date}`)
-    .then(response => response.json())
-    .then(data => {
-        // Suppression contenu css pour ajout traitement données
-        resultAll.forEach(item => {
-            while (item.firstChild) {
-                item.removeChild(item.firstChild);
-            }
-        });
+        .then(response => response.json())
+        .then(data => {
+            // Suppression contenu css pour ajout traitement données
+            resultAll.forEach(item => {
+                while (item.firstChild) {
+                    item.removeChild(item.firstChild);
+                }
+            });
 
-        // Traiter les données de réponse
-        if (data) {
-            console.log(data);
-            data.forEach(trip => {
-                // Créer des éléments HTML pour afficher chaque trajet
-                result.innerHTML += `
+            // Traiter les données de réponse
+            if (data) {
+                console.log(data);
+                data.forEach(trip => {
+                    // Créer des éléments HTML pour afficher chaque trajet
+                    result.innerHTML += `
                     <div id="result">
                     <span id="trip">${departure} > ${arrival}</span>
                     <span id="time">16:23</span>
                     <span id="price">126€</span>
                     <button id="book-button">Book</button>
                     </div>`;
-            });
-        }
-    })
+                });
+            } else {
+                result.innerHTML = `
+                    <div id="trip-notfound">
+                    <div>
+                    <img id="notfound-image" src="images/notfound.png">
+                    </div> 
+                    <hr id="green-line">
+                    <p>No trip found</p>
+                    </div>`;
+            }
+        })
 
 });
 
